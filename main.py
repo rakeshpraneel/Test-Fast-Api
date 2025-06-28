5be7cfa9a542116ed19a2d479781cb12850ebca6
Rakesh <rakeshpraneel@gmail.com>
2025-06-28 13:00:57 +0530

    Added mock fast api

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, HTTPException, Form
+
+app = FastAPI()
+client_secret_return = ""
+
+@app.post("/postmethod")
+async def upload(
+    client_id: str = Form(),
+    client_secret: int = Form(),
+    grant_type: str = Form()):
+    if client_secret == 'Nothing':
+        return {
+            'client_id': client_id,
+            'access_token': 'story of black water',
+            'grant_type': grant_type
+        }
+    else:
+        raise HTTPException(status_code=400, detail="wrong input data")
+
+@app.get("/getmethod")
+async def download():
+    return client_secret_return
